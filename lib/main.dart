d853df787117a9a965c41232872ba8447df903c2
Jeroen Slemmer <jeroen.slemmer@gmail.com>
2026-07-28 23:04:30 +0200

    feat: add Riverpod foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'core/firebase/firebase_service.dart';
 import 'features/auth/auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await FirebaseService.init();
-  runApp(const MyApp());
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
