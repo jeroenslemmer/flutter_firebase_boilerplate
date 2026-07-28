415641976da26d5df3047b16afc57b864a20d44a
Jeroen Slemmer <jeroen.slemmer@gmail.com>
2026-07-28 21:17:04 +0200

    refactor: extract AuthGate from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'core/firebase/firebase_service.dart';
-import 'features/home/home_screen.dart';
-import 'features/auth/login_screen.dart';
+import 'features/auth/auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,27 +10,11 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-
-          if (snapshot.hasData) {
-            return HomeScreen();
-          } else {
-            return const LoginScreen();
-          }
-        },
-      ),
+      home: const AuthGate(),
     );
   }
 }
